c6d55b5b0582fc7c40e1bff9abb5ec04f4c5e000
Nebojsa Jakovljevic <nebojsa@nebjak.net>
2024-08-22 18:25:54 +0200

    feat: organize code better

--- greet/greet.go
+++ greet/greet.go
@@ -2,6 +2,12 @@ package greet
 
 import "fmt"
 
+// SayHello returns a greeting message
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello, %s!", name)
+}
+
+// PrintHello prints a hello message
+func PrintHello(name string) {
+	fmt.Println(SayHello(name))
 }
